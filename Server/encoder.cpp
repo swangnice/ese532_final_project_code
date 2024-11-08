e16565d1e4b4f77cd03cf41ccd2436cee7cecfc5
shibo <swangnice@outlook.com>
2024-11-08 12:13:18 -0500

    first commit

--- Server/encoder.cpp
+++ Server/encoder.cpp
@@ -219,8 +219,7 @@ int main(int argc, char* argv[]) {
 	}
 	for (unsigned int i = 0; i < chunk_count; i++) {
 		printf("Chunk %u: ", i);
-		printf("%.*s\n", chunk_sizes[i], (char *)chunks[i]);
-		printf("Header: %u\n", header[i]);
+		printf("Header: %#010x\n", header[i]);
 	}
 
 
