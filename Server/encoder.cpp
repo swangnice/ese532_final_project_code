f2552534ac9f3ab3b4aa67b65e86d6651d929006
shibo <swangnice@outlook.com>
2024-11-13 21:10:42 -0500

    update

--- Server/encoder.cpp
+++ Server/encoder.cpp
@@ -302,9 +302,11 @@ int main(int argc, char* argv[]) {
     }
 	for (unsigned int i = 0; i < chunk_count; i++) {
 		fwrite(&header[i], sizeof(uint32_t), 1, out_file);
+		printf("%#010x", i, header[i]);
 		if (dup_flag[i] == 0) {
 			for (unsigned int j = 0; j < compressed_data_size[i]; j++) {
 				fwrite(&lzw_compressed_output[i][j], sizeof(uint8_t), 1, out_file);
+				printf("%02X ", lzw_compressed_output[i][j]);
 			}
 		}
 	}
