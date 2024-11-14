8ffc28ca3e7575c5406f091927876550035291b0
shibo <swangnice@outlook.com>
2024-11-14 10:59:45 -0500

    update

--- Server/lzw.cpp
+++ Server/lzw.cpp
@@ -171,44 +171,225 @@ void lookup(unsigned long* hash_table, assoc_mem* mem, unsigned int key, bool* h
 }
 //****************************************************************************************************************
 
-std::vector<int> encoding(const unsigned char* data, size_t size) {
-    //std::cout << "Encoding\n";
-    std::string s1(reinterpret_cast<const char*>(data), size);
-    std::unordered_map<std::string, int> table;
-    for (int i = 0; i <= 255; i++) {
-        std::string ch = "";
-        ch += char(i);
-        table[ch] = i;
+void hardware_encoding(unsigned char* s1, int length, uint16_t* out_code, uint32_t &header, int &out_len)
+{
+    // create hash table and assoc mem
+    unsigned long hash_table[CAPACITY];
+    assoc_mem my_assoc_mem;
+
+    // make sure the memories are clear
+    for(int i = 0; i < CAPACITY; i++)
+    {
+        hash_table[i] = 0;
+    }
+    my_assoc_mem.fill = 0;
+    for(int i = 0; i < 512; i++)
+    {
+        my_assoc_mem.upper_key_mem[i] = 0;
+            my_assoc_mem.middle_key_mem[i] = 0;
+        my_assoc_mem.lower_key_mem[i] = 0;
     }
 
-    std::string p = "", c = "";
-    p += s1[0];
-    int code = 256;
-    std::vector<int> output_code;
+    // init the memories with the first 256 codes
+    for(unsigned long i = 0; i < 256; i++)
+    {
+        bool collision = 0;
+        unsigned int key = (i << 8) + 0UL; // lower 8 bits are the next char, the upper bits are the prefix code
+        insert(hash_table, &my_assoc_mem, key, i, &collision);
+    }
+    int next_code = 256;
 
-    //std::cout << "String\tOutput_Code\tAddition\n";
-    for (size_t i = 0; i < s1.length(); i++) {
-        if (i != s1.length() - 1)
-            c += s1[i + 1];
-        if (table.find(p + c) != table.end()) {
-            p = p + c;
-        } else {
-            //std::cout << p << "\t" << table[p] << "\t\t"<< p + c << "\t" << code << std::endl;
-            output_code.push_back(table[p]);
-            table[p + c] = code;
-            code++;
-            p = c;
+
+    int prefix_code = s1[0];
+    unsigned int code = 0;
+    char next_char = 0;
+
+    int i = 0, j = 0;
+    while(i < length)
+    {
+        // if(i + 1 == length)
+        // {
+        //     //std::cout << prefix_code;
+        //     //std::cout << "\n";
+        //     // i++;
+        //     break;
+        // }
+        next_char = s1[i + 1];
+
+        bool hit = 0;
+        //std::cout << "prefix_code " << prefix_code << " next_char " << next_char << std::endl;
+        lookup(hash_table, &my_assoc_mem, (prefix_code << 8) + next_char, &hit, &code);
+        if(!hit)
+        {
+            //std::cout << prefix_code;
+            out_code[j++] = prefix_code;
+            // out_code[i]=prefix_code;
+            //std::cout << "\n";
+
+            bool collision = 0;
+            insert(hash_table, &my_assoc_mem, (prefix_code << 8) + next_char, next_code, &collision);
+            if(collision)
+            {
+                std::cout << "ERROR: FAILED TO INSERT! NO MORE ROOM IN ASSOC MEM!" << std::endl;
+                return;
+            }
+            next_code += 1;
+
+            prefix_code = next_char;
         }
-        c = "";
+        else
+        {
+            prefix_code = code;
+            if(i + 1 == length)
+                out_code[j++] = prefix_code;
+        }
+        i += 1;
+    }
+    out_len = j;
+    // write_encoded_file(out_code, out_len, header, outputFile);
+
+    // header = static_cast<uint32_t>(out_len) << 1;
+    
+    //std::cout << std::endl << "assoc mem entry count: " << my_assoc_mem.fill << std::endl;
+
+    // std::ofstream outfile("encoded_data.bin", std::ios::binary);
+    // if (!outfile) {
+    //     std::cerr << "Could not open the file for writing." << std::endl;
+    //     return;
+    // }
+    // outfile.write(reinterpret_cast<const char*>(&header), sizeof(header));
+    // for (int i = 0; i < out_len; ++i) {
+    //     outfile.write(reinterpret_cast<const char*>(&out_code[i]), sizeof(uint16_t));
+    // }
+    // outfile.close();
+} 
+//hardware
+//****************************************************************************************************************
+void hardware_encoding(unsigned char* s1, int length, uint16_t* out_code, int &out_len)
+{
+    // create hash table and assoc mem
+    unsigned long hash_table[CAPACITY];
+    assoc_mem my_assoc_mem;
+
+    // make sure the memories are clear
+    for(int i = 0; i < CAPACITY; i++)
+    {
+        hash_table[i] = 0;
+    }
+    my_assoc_mem.fill = 0;
+    for(int i = 0; i < 512; i++)
+    {
+        my_assoc_mem.upper_key_mem[i] = 0;
+            my_assoc_mem.middle_key_mem[i] = 0;
+        my_assoc_mem.lower_key_mem[i] = 0;
     }
 
-    output_code.push_back(table[p]);
+    // init the memories with the first 256 codes
+    for(unsigned long i = 0; i < 256; i++)
+    {
+        bool collision = 0;
+        unsigned int key = (i << 8) + 0UL; // lower 8 bits are the next char, the upper bits are the prefix code
+        insert(hash_table, &my_assoc_mem, key, i, &collision);
+    }
+    int next_code = 256;
 
-    return output_code;
+
+    int prefix_code = s1[0];
+    unsigned int code = 0;
+    char next_char = 0;
+
+    int i = 0, j = 0;
+    while(i < length)
+    {
+        // if(i + 1 == length)
+        // {
+        //     //std::cout << prefix_code;
+        //     //std::cout << "\n";
+        //     // i++;
+        //     break;
+        // }
+        next_char = s1[i + 1];
+
+        bool hit = 0;
+        //std::cout << "prefix_code " << prefix_code << " next_char " << next_char << std::endl;
+        lookup(hash_table, &my_assoc_mem, (prefix_code << 8) + next_char, &hit, &code);
+        if(!hit)
+        {
+            //std::cout << prefix_code;
+            out_code[j++] = prefix_code;
+            // out_code[i]=prefix_code;
+            //std::cout << "\n";
+
+            bool collision = 0;
+            insert(hash_table, &my_assoc_mem, (prefix_code << 8) + next_char, next_code, &collision);
+            if(collision)
+            {
+                std::cout << "ERROR: FAILED TO INSERT! NO MORE ROOM IN ASSOC MEM!" << std::endl;
+                return;
+            }
+            next_code += 1;
+
+            prefix_code = next_char;
+        }
+        else
+        {
+            prefix_code = code;
+            if(i + 1 == length)
+                out_code[j++] = prefix_code;
+        }
+        i += 1;
+    }
+    out_len = j;
+    // write_encoded_file(out_code, out_len, header, outputFile);
+
+    // header = static_cast<uint32_t>(out_len) << 1;
+    
+    //std::cout << std::endl << "assoc mem entry count: " << my_assoc_mem.fill << std::endl;
+
+    // std::ofstream outfile("encoded_data.bin", std::ios::binary);
+    // if (!outfile) {
+    //     std::cerr << "Could not open the file for writing." << std::endl;
+    //     return;
+    // }
+    // outfile.write(reinterpret_cast<const char*>(&header), sizeof(header));
+    // for (int i = 0; i < out_len; ++i) {
+    //     outfile.write(reinterpret_cast<const char*>(&out_code[i]), sizeof(uint16_t));
+    // }
+    // outfile.close();
 }
 
 
+int convert_output(uint16_t in[], uint8_t out[], int input_size){
+    //header
+    int output_size = 0;
 
+    int adjusted_input_size = input_size - (input_size % 2);
+
+    for(int i = 0; i < adjusted_input_size; i+=2){
+        //printf("in[i]: %hu\n", in[i]);
+        //printf("in[i+1]: %hu\n", in[i+1]);
+        
+
+        out[output_size] = (in[i]>>4) & 0xff;
+        //printf("out[1]: %u\n", static_cast<unsigned int>(out[output_size]));
+        output_size++;
+        out[output_size] = ((in[i] << 4) & 0xf0) | ((in[i+1] >> 8) & 0x0f);
+        //printf("out[2]: %u\n", static_cast<unsigned int>(out[output_size]));
+        output_size++;
+        out[output_size] = (in[i+1]) & 0xff;
+        //printf("out[3]: %u\n", static_cast<unsigned int>(out[output_size]));
+        output_size++;
+    }
+
+    if (input_size % 2 != 0) {
+        out[output_size] = (in[adjusted_input_size] >> 4) & 0xFF;
+        output_size++;
+        out[output_size] = (in[adjusted_input_size] << 4) & 0xF0;
+        output_size++;
+    }
+
+    return output_size;
+}
 
 
 // void decoding(std::vector<int> op)
