946d2544b7876345e0174a86b0028895a856a878
shibo <swangnice@outlook.com>
2024-11-22 13:48:58 -0500

    update

--- hls/host.cpp
+++ hls/host.cpp
@@ -260,7 +260,7 @@ int main(int argc, char** argv)
             //lzw_compress(chunks[i], &chunk_sizes[i], temp_lzw_compressed_output[i], &temp_output_index[i]);
             memcpy(&lzw_s1[i], chunks[i], chunk_sizes[i]);
             lzw_length = &chunk_sizes[i];
-
+			printf("begin to set Arg\n");
             lzw_kernel.setArg(0, lzw_s1_buf);
             lzw_kernel.setArg(1, lzw_length_buf);
             lzw_kernel.setArg(2, lzw_out_code_buf);
