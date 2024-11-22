47e5093928eb21f6413743cb4cd0a4e128166830
shibo <swangnice@outlook.com>
2024-11-22 14:09:29 -0500

    update

--- hls/host.cpp
+++ hls/host.cpp
@@ -253,10 +253,7 @@ int main(int argc, char** argv)
     //     memcpy(&lzw_s1[i * MAX_CHUNK_SIZE], chunks[i], chunk_sizes[i]);
     //     lzw_length[i] = chunk_sizes[i];
     // }
-            lzw_kernel.setArg(0, lzw_s1_buf);
-            lzw_kernel.setArg(1, lzw_length_buf);
-            lzw_kernel.setArg(2, lzw_out_code_buf);
-            lzw_kernel.setArg(3, lzw_out_len_buf);
+
 
     for (unsigned int i = 0; i < chunk_count; i++) {
         if (dup_flag[i] == 0) {
@@ -266,7 +263,10 @@ int main(int argc, char** argv)
 
 			printf("begin to write in buffer\n");
 
-
+			lzw_kernel.setArg(0, lzw_s1_buf);
+            lzw_kernel.setArg(1, lzw_length_buf);
+            lzw_kernel.setArg(2, lzw_out_code_buf);
+            lzw_kernel.setArg(3, lzw_out_len_buf);
 
             std::vector<cl::Event> write_events;
             std::vector<cl::Event> exec_events;
