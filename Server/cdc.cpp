ff5527510b7af3592b9ef96e8ecc452c0dad5ab4
shibo <swangnice@outlook.com>
2024-12-06 10:39:11 -0500

    update code 2024-12-06 10:39:11

--- Server/cdc.cpp
+++ Server/cdc.cpp
@@ -53,21 +53,21 @@ void gear_based_fastcdc(const unsigned char *buff, unsigned int buff_size, unsig
         return;
     }
 
-    unsigned int size = 0;
+    unsigned int chunk_size = 0;
     while (i < buff_size) {
         current_hash = gear_rolling_hash(current_hash, buff[i]);
-        if ((size < FASTCDC_AVG_CHUNK && ((current_hash & mask_small) == 0)) /*small chunk*/|| (size >= FASTCDC_AVG_CHUNK && ((current_hash & mask_large) == 0)) /*large chunk*/|| (size == FASTCDC_MAX_CHUNK - 1)/*Max Chunk */|| (i == buff_size-1)/*remain part*/) {
-            unsigned int chunk_size = i - start + 1;
+        if ((chunk_size < FASTCDC_AVG_CHUNK && ((current_hash & mask_small) == 0)) /*small chunk*/|| (chunk_size >= FASTCDC_AVG_CHUNK && ((current_hash & mask_large) == 0)) /*large chunk*/|| (chunk_size == FASTCDC_MAX_CHUNK - 1)/*Max Chunk */|| (i == buff_size-1)/*remain part*/) {
+            //unsigned int chunk_size = i - start + 1;
             if (chunk_size >= FASTCDC_MIN_CHUNK) {
                 (*chunks)[*chunk_count] = (unsigned char *)malloc(chunk_size);
                 memcpy((*chunks)[*chunk_count], buff + start, chunk_size);
                 (*chunk_sizes)[*chunk_count] = chunk_size;
                 (*chunk_count)++;
                 start = i + 1;
-                size = 0;
+                buff_size = 0;
             }
         }
         i++;
-        size++;
+        chunk_size++;
     }
 }
