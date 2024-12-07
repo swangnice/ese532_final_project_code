b1f71498cdea2816e0faab7213cd483cb316eca8
shibo <swangnice@outlook.com>
2024-12-07 10:47:05 -0500

    update code 2024-12-07 10:47:05

--- Server/encoder.cpp
+++ Server/encoder.cpp
@@ -253,8 +253,8 @@ int main(int argc, char* argv[]) {
     unsigned int temp_out_buffer_size = 0;
     size_t out_offset = 0;
     for (unsigned int i = 0; i < chunk_count; i++) {
+        memset(temp_out_buffer, 0, 1024 * 2);
         lzw_compress_v2(chunks[i], &chunk_sizes[i], dup_flag[i], dup_index[i], temp_out_buffer, temp_out_buffer_size);
-
         memcpy(out_buffer + out_offset, temp_out_buffer, temp_out_buffer_size);
         out_offset += temp_out_buffer_size;
     }
