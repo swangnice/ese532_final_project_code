4eb43025890b79aefea949b852a346b464356931
shibo <swangnice@outlook.com>
2024-12-07 10:50:28 -0500

    update code 2024-12-07 10:50:28

--- Server/lzw.cpp
+++ Server/lzw.cpp
@@ -223,6 +223,7 @@ void lzw_compress(unsigned char* s1, int* length, uint16_t* out_code, int *out_l
 
 void lzw_compress_v2(unsigned char* s1, int* length, uint8_t is_dup, int dup_index,  uint8_t *temp_out_buffer, unsigned int temp_out_buffer_size)
 {
+    printf("Begin lzw_compress_v2\n");
     if (!is_dup){
         unsigned long hash_table[CAPACITY];
         assoc_mem my_assoc_mem;
@@ -276,6 +277,7 @@ void lzw_compress_v2(unsigned char* s1, int* length, uint8_t is_dup, int dup_ind
             }
             i += 1;
         }
+        printf("End lzw_compress\n");
         //*out_len = j;
         // Convert Output
         int output_size = 0;
@@ -301,12 +303,14 @@ void lzw_compress_v2(unsigned char* s1, int* length, uint8_t is_dup, int dup_ind
             temp_out_buffer[output_size+4] = (out_code[adjusted_input_size] << 4) & 0xF0;
             output_size++;
         }
+    printf("End convert_output\n");
         // Generate header and combine with output
         temp_out_buffer[0] = (output_size << 1) & 0xff000000;
         temp_out_buffer[1] = (output_size << 1) & 0x00ff0000;
         temp_out_buffer[2] = (output_size << 1) & 0x0000ff00;
         temp_out_buffer[3] = (output_size << 1) & 0x000000ff;
         temp_out_buffer_size = output_size + 4;
+    printf("End build buffer\n");
     } else {
         // Generate header and combine with output
         temp_out_buffer[0] = ((dup_index<<1) | 0x00000001) & 0xff000000;
